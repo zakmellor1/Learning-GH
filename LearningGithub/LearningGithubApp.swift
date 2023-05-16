0f796cc7b15d1d744365b2b2eb92f2a6a39e813c
Zak Mellor <zakmellor@me.com>
2023-05-16 16:44:52 +0100

    Initial Commit

---
+++ LearningGithub/LearningGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGithubApp.swift
+//  LearningGithub
+//
+//  Created by Zak Mellor on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
