926630efa32301b77d4ff5716ce653885b33082d
zakmellor1 <120800771+zakmellor1@users.noreply.github.com>
2023-05-16 17:05:33 +0100

    Update ContentView.swift to Hey

--- LearningGithub/ContentView.swift
+++ LearningGithub/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, GitHub!")
+            Text("Hey, GitHub!")
         }
         .padding()
     }
